a0eaf2e9a405d1033f8bd0600365c6bc84780ba2
vasek.gryc <vaclav.gryc@topmonks.com>
2023-11-22 09:16:11 +0100

    added browser version

---
+++ dist/napoveda/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4AJZBQQM.mjs";import"./chunk-KH4RF47Q.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [provideRouter(routes), provideAnimations(), provideClientHydration()]
 };
